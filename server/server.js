c5d89c5baae9bfd37db1941926827fa551858ffe
dansilvaUT <dan13silv@gmail.com>
2021-06-29 20:03:25 -0700

    backend set up with DB, session and environment variables

--- server/server.js
+++ server/server.js
@@ -1,4 +1,27 @@
+require('dotenv').config();
 const express = require('express'),
+    session = require('express-session'),
+    massive = require('massive'),
+    { PORT, SESSION_SECRET, CONNECTION_STRING } = process.env,
     app = express();
 
-app.listen(4444, () => console.log('Listening on port 4444'));
+//Middleware
+app.use(express.json());
+app.use(session({
+    resave: false,
+    saveUninitialized: true,
+    secret: SESSION_SECRET,
+    cookie: { maxAge: 1000 * 60 * 60 * 24 * 365 }
+}));
+
+//Connect to DB
+massive({
+    connectionString: CONNECTION_STRING,
+    ssl: { rejectUnauthorized: false }
+}).then(db => {
+    app.set('db', db);
+    console.log('RoundTableReview DB connected');
+}).catch(err => console.log(`DB Error ${err.message}`));
+
+
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
