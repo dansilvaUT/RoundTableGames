f0ead297f3de5045fa4360a59d7b398101cb1f67
dansilvaUT <dan13silv@gmail.com>
2021-07-02 21:57:36 -0600

    auth controller tested and workig corretly

--- server/controllers/auth/authController.js
+++ server/controllers/auth/authController.js
@@ -3,8 +3,7 @@ const bcrypt = require('bcryptjs');
 module.exports = {
     register: async (req, res) => {
         const { email, username, password } = req.body;
-        const db = req.app('db');
-
+        const db = req.app.get('db');
         const [foundUser] = await db.users.check_user({ username, email });
 
         if (foundUser && foundUser.username === username) {
@@ -23,6 +22,39 @@ module.exports = {
         req.session.user = newUser;
         delete req.session.user.password;
 
-        res.status(201).send(newUser);
+        res.status(201).send(req.session.user);
+    },
+
+    login: async (req, res) => {
+        const { username, password } = req.body;
+        const db = req.app.get('db');
+        const [foundUser] = await db.users.get_user({ username });
+
+        if (!foundUser) {
+            return res.status(404).send(`No user with username ${username} found`);
+        }
+
+        const authenticated = bcrypt.compareSync(password, foundUser.password);
+
+        if (!authenticated) {
+            return res.status(401).send(`Incorrect password for ${username}`);
+        }
+
+        delete foundUser.password;
+        req.session.user = foundUser;
+        res.status(200).send(req.session.user);
+    },
+
+    getUser: (req, res) => {
+        if (!req.session.user) {
+            return res.status(401).send('No user found, please login.');
+        }
+
+        res.status(200).send(req.session.user);
+    },
+
+    logout: (req, res) => {
+        req.session.destroy();
+        res.status(200).send('Successfully logged out');
     }
 }
