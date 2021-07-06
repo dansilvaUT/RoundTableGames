eee29629c90845745badcffabbe5157aa5f8d9c2
dansilvaUT <dan13silv@gmail.com>
2021-07-06 15:42:16 -0700

    toggle login field

---
+++ client/Screens/Landing/Login/Login.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, StyleSheet, TextInput } from 'react-native';
+const Login = () => {
+    return (
+        <View>
+            <TextInput />
+        </View>
+    )
+}
+
+export default Login;
+
